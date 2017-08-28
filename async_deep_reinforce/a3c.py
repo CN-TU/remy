Correct way the score is shown. Clean up code base by removing the non-LSTM implementation. Backing up the current LSTM state before calculating the gradient

async_deep_reinforce/a3c.py
@@ -28,7 +28,6 @@ from constants import RMSP_EPSILON
 from constants import RMSP_ALPHA
 from constants import GRAD_NORM_CLIP
 from constants import USE_GPU
-from constants import USE_LSTM
 from constants import PRECISION
 from constants import ALPHA, BETA
 from constants import LOG_LEVEL
@@ -63,10 +62,7 @@ global_t = 0
 
 stop_requested = False
 
-if USE_LSTM:
-  global_network = GameACLSTMNetwork(-1, device)
-else:
-  global_network = GameACFFNetwork(-1, device)
+global_network = GameACLSTMNetwork(-1, device)
 
 learning_rate_input = tf.placeholder(PRECISION)
 
@@ -212,7 +208,7 @@ def call_process_reward(thread_id, reward_throughput, reward_delay, duration):
   global_t += diff_global_t
 
 def call_process_finished(thread_id, actions_to_remove, time_difference):
-  logging.debug(" ".join(map(str,("call_process_finished", thread_id))))
+  logging.debug(" ".join(map(str,("call_process_finished", thread_id, time_difference))))
   global sess, global_t, summary_writer, summary_op, summary_inputs
   diff_global_t = training_threads[thread_id].final_step(sess, global_t, summary_writer, summary_op, summary_inputs, actions_to_remove, time_difference)
   global_t += diff_global_t

async_deep_reinforce/a3c_training_thread.py
@@ -15,7 +15,6 @@ from constants import GAMMA
 # gamma_current, gamma_future = 1./(1+GAMMA), GAMMA/(1.+GAMMA)
 from constants import LOCAL_T_MAX
 from constants import ENTROPY_BETA
-from constants import USE_LSTM
 from constants import ALPHA, BETA
 from constants import LOG_LEVEL
 
@@ -48,10 +47,7 @@ class A3CTrainingThread(object):
     self.learning_rate_input = learning_rate_input
     self.max_global_time_step = max_global_time_step
 
-    if USE_LSTM:
-      self.local_network = GameACLSTMNetwork(thread_index, device)
-    else:
-      self.local_network = GameACFFNetwork(thread_index, device)
+    self.local_network = GameACLSTMNetwork(thread_index, device)
 
     self.local_network.prepare_loss(ENTROPY_BETA)
 
@@ -62,26 +58,16 @@ class A3CTrainingThread(object):
         gate_gradients=False,
         aggregation_method=None,
         colocate_gradients_with_ops=False)
-
-      # self.grads_and_vars = grad_applier.compute_gradients(
-      #   self.local_network.total_loss,
-      #   self.local_network.get_vars(),
-      #   gate_gradients=False,
-      #   aggregation_method=None,
-      #   colocate_gradients_with_ops=False,
-      # )
-
-    # self.apply_gradients = grad_applier.apply_gradients(
-    #   global_network.get_vars(),
-    #   self.gradients )
       
     self.apply_gradients = grad_applier.apply_gradients(
       zip(self.gradients, global_network.get_vars()) )
     
     self.sync = self.local_network.sync_from(global_network)
+
+    self.backup_vars = self.local_network.backup_vars()
+    self.restore_backup = self.local_network.restore_backup()
         
     self.local_t = 0
-    self.episode_start_t = 0
 
     self.initial_learning_rate = initial_learning_rate
 
@@ -120,13 +106,11 @@ class A3CTrainingThread(object):
     self.start_time = start_time
 
   def action_step(self, sess, state):
-
     if len(self.actions) % LOCAL_T_MAX == 0:
       # Sync for the next iteration
       sess.run( self.sync )
-      if USE_LSTM:
-        self.start_lstm_states.append(self.local_network.lstm_state_out)
-        self.variable_snapshots.append(tuple(map(sess.run, self.local_network.get_vars())))
+      self.start_lstm_states.append(self.local_network.lstm_state_out)
+      self.variable_snapshots.append(sess.run(self.local_network.get_vars()))
 
     pi_, action, value_ = self.local_network.run_policy_action_and_value(sess, state)
 
@@ -150,25 +134,25 @@ class A3CTrainingThread(object):
     assert(duration >= 0)
     self.durations.append(duration)
 
-    if len(self.rewards)>=LOCAL_T_MAX:
+    if len(self.rewards)>LOCAL_T_MAX:
       return self.process(sess, global_t, summary_writer, summary_op, summary_inputs)
     else:
       return  0
-    # implicitly returns None otherwise
 
   def final_step(self, sess, global_t, summary_writer, summary_op, summary_inputs, actions_to_remove, time_difference):
     self.actions = self.actions[:-actions_to_remove]
     self.states = self.states[:-actions_to_remove]
     self.values = self.values[:-actions_to_remove]
 
-    if len(self.rewards) > 0:
-      return self.process(sess, global_t, summary_writer, summary_op, summary_inputs, time_difference)
+    # If, for some strange reason, absolutely nothing happened in this episode, don't do anyting...
+    if len(self.rewards)>0:
+      return self.process(sess, global_t, summary_writer, summary_op, summary_inputs, time_difference=time_difference)
     else:
       return 0
 
   def process(self, sess, global_t, summary_writer, summary_op, summary_inputs, time_difference=None):
-
     final = time_difference is not None
+
     start_local_t = self.local_t
     
     logging.debug(" ".join(map(str,(self.thread_index, "In process: len(rewards)", len(self.rewards), "len(durations)", len(self.durations), "len(states)", len(self.states), "len(actions)", len(self.actions), "len(values)", len(self.values)))))
@@ -181,21 +165,9 @@ class A3CTrainingThread(object):
 
     logging.debug(" ".join(map(str,(self.thread_index, "In process: rewards", rewards, "durations", durations, "states", states, "actions", actions, "values", values))))
 
-    # t_max times loop
-    # for i in range(len(rewards)):
-    #   self.local_t += 1
-
-    self.local_t += len(rewards)
-
-    # return advanced local step size
-    diff_local_t = self.local_t - start_local_t
-
-    if final:
-      log_str = "final"
-    else:
-      log_str = "intermediate"
-    R_packets, R_accumulated_delay, R_duration = self.local_network.run_value(sess, states[-1])
-    logging.debug(log_str+": "+" ".join(map(str,("R_packets", R_packets, "R_accumulated_delay", R_accumulated_delay, "R_duration", R_duration))))
+    # get estimated value of step n+1
+    R_packets, R_accumulated_delay, R_duration = self.values[len(rewards)] if len(self.values) > len(rewards) else self.values[-1]
+    logging.debug("final:"+str(final)+" ".join(map(str,("R_packets", R_packets, "R_accumulated_delay", R_accumulated_delay, "R_duration", R_duration))))
 
     R_packets, R_accumulated_delay, R_duration = (R_packets)/(1-GAMMA), (R_accumulated_delay)/(1-GAMMA), (R_duration)/(1-GAMMA)
     # logging.debug(" ".join(map(str,("exp(R_packets)", R_packets, "exp(R_accumulated_delay)", R_accumulated_delay, "exp(R_duration)", R_duration))))
@@ -224,8 +196,6 @@ class A3CTrainingThread(object):
       R_duration = (di + GAMMA*R_duration)
 
       R_packets = (ri[0] + GAMMA*R_packets)
-      # R_throughput = R_packets/R_duration
-      # FIXME: Put assertions here
       # TODO: In theory it should be np.log(R_bytes/R_duration) but remy doesn't have bytes
       td_throughput = (np.log(R_packets/R_duration) - np.log(Vi[0]/Vi[2]))
 
@@ -249,69 +219,96 @@ class A3CTrainingThread(object):
       self.episode_reward_throughput += ri[0]
       self.episode_reward_delay += ri[1]
 
+    self.local_t += len(rewards)
+
+    # return advanced local step size
+    diff_local_t = self.local_t - start_local_t
+
     cur_learning_rate = self._anneal_learning_rate(global_t)
 
     # logging.debug(" ".join(map(str,("All the batch stuff", "batch_si", batch_si, "batch_ai", batch_ai, "batch_td_throughput", batch_td_throughput, "batch_td_delay", batch_td_delay,"batch_R_packets", batch_R_packets, "batch_R_accumulated_delay", batch_R_accumulated_delay, "batch_R_duration", batch_R_duration))))
 
-    if USE_LSTM:
-      batch_si.reverse()
-      batch_ai.reverse()
-      batch_td_throughput.reverse()
-      batch_td_delay.reverse()
-      batch_R_duration.reverse()
-      batch_R_packets.reverse()
-      batch_R_accumulated_delay.reverse()
+    self.backup_vars(sess)
 
-      variables_backup = tuple(map(sess.run, self.local_network.get_vars()))
+    batch_si.reverse()
+    batch_ai.reverse()
+    batch_td_throughput.reverse()
+    batch_td_delay.reverse()
+    batch_R_duration.reverse()
+    batch_R_packets.reverse()
+    batch_R_accumulated_delay.reverse()
 
-      for var, value in zip(self.local_network.get_vars(), self.variable_snapshots[0]):
-        sess.run(var.assign(val))
+    feed_dict = {
+      self.local_network.s: batch_si,
+      self.local_network.a: batch_ai,
+      self.local_network.td_throughput: batch_td_throughput,
+      self.local_network.td_delay: batch_td_delay,
+      self.local_network.r_duration: batch_R_duration,
+      self.local_network.r_packets: batch_R_packets,
+      self.local_network.r_accumulated_delay: batch_R_accumulated_delay,
+      self.local_network.initial_lstm_state: self.start_lstm_states[0],
+      self.local_network.step_size : [len(batch_ai)],
+      self.learning_rate_input: cur_learning_rate
+    }
+    var_dict = dict(zip(self.local_network.get_vars(), self.variable_snapshots[0]))
+    feed_dict.update(var_dict)
 
-      sess.run( self.apply_gradients,
-                feed_dict = {
-                  self.local_network.s: batch_si,
-                  self.local_network.a: batch_ai,
-                  self.local_network.td_throughput: batch_td_throughput,
-                  self.local_network.td_delay: batch_td_delay,
-                  self.local_network.r_duration: batch_R_duration,
-                  self.local_network.r_packets: batch_R_packets,
-                  self.local_network.r_accumulated_delay: batch_R_accumulated_delay,
-                  self.local_network.initial_lstm_state: self.start_lstm_states[0],
-                  self.local_network.step_size : [len(batch_ai)],
-                  self.learning_rate_input: cur_learning_rate } )
-                
-      for var, value in zip(self.local_network.get_vars(), variables_backup):
-        sess.run(var.assign(val))
-    else:
-      raise NotImplementedError("FF currently not implemented.")
+    sess.run( self.apply_gradients,
+              feed_dict = feed_dict )
 
     if final:
-      normalized_final_score_throughput = self.episode_reward_throughput/time_difference
-      normalized_final_score_delay = self.episode_reward_delay/self.episode_reward_throughput
-      logging.debug("{}: score_throughput={}, score_delay={}".format(self.thread_index, normalized_final_score_throughput, normalized_final_score_delay))
-      entropy, actor_loss, value_loss, total_loss, window, std = self.local_network.run_loss(sess, batch_si[-1], batch_ai[-1], batch_td_throughput[-1], batch_td_delay[-1], batch_R_duration[-1], batch_R_packets[-1], batch_R_accumulated_delay[-1])
-      things = {"score_throughput": normalized_final_score_throughput, 
-        "score_delay": normalized_final_score_delay, 
-        "actor_loss": actor_loss.item(),
-        "value_loss": value_loss,
-        "entropy": entropy.item(),
-        "total_loss": total_loss,
-        "window": window.item(),
-        "std": std.item()}
-      logging.debug(" ".join(map(str,("things", things))))
-      self._record_score(sess, summary_writer, summary_op, summary_inputs, things, global_t) # TODO:NOW: is that "not terminal_end" correct?
-      self.episode_start_t = self.local_t
+      if time_difference > 0 and self.episode_reward_throughput > 0 and self.episode_reward_delay > 0:
+        normalized_final_score_throughput = self.episode_reward_throughput/time_difference
+        normalized_final_score_delay = self.episode_reward_delay/self.episode_reward_throughput
+        logging.debug("{}: score_throughput={}, score_delay={}".format(self.thread_index, normalized_final_score_throughput, normalized_final_score_delay))
+
+        feed_dict = {
+          self.local_network.s: [batch_si[0]],
+          self.local_network.a: [batch_ai[0]],
+          self.local_network.td_throughput: [batch_td_throughput[0]],
+          self.local_network.td_delay: [batch_td_delay[0]],
+          self.local_network.r_duration: [batch_R_duration[0]],
+          self.local_network.r_packets: [batch_R_packets[0]],
+          self.local_network.r_accumulated_delay: [batch_R_accumulated_delay[0]],
+          self.local_network.initial_lstm_state: self.start_lstm_states[0],
+          self.local_network.step_size : [1],
+          self.learning_rate_input: cur_learning_rate
+        }
+        feed_dict.update(var_dict)
+
+        entropy, actor_loss, value_loss, total_loss, window, std = self.local_network.run_loss(sess, feed_dict)
+
+        things = {"score_throughput": normalized_final_score_throughput, 
+          "score_delay": normalized_final_score_delay, 
+          "actor_loss": actor_loss.item(),
+          "value_loss": value_loss,
+          "entropy": entropy.item(),
+          "total_loss": total_loss,
+          "window": window.item(),
+          "std": std.item()}
+        logging.debug(" ".join(map(str,("things", things))))
+        self._record_score(sess, summary_writer, summary_op, summary_inputs, things, global_t) # TODO:NOW: is that "not terminal_end" correct?
+
+        elapsed_time = time.time() - self.start_time
+        steps_per_sec = self.local_t / elapsed_time
+        logging.info("### {}: Performance: {} STEPS in {:.0f} sec. {:.0f} STEPS/sec. {:.2f}M STEPS/hour".format(self.thread_index, self.local_t, elapsed_time, steps_per_sec, steps_per_sec * 3600 / 1000000.))
+
       self.episode_reward_throughput = 0
       self.episode_reward_delay = 0
 
-      elapsed_time = time.time() - self.start_time
-      steps_per_sec = self.local_t / elapsed_time
-      logging.info("### {}: Performance: {} STEPS in {:.0f} sec. {:.0f} STEPS/sec. {:.2f}M STEPS/hour".format(self.thread_index, self.local_t, elapsed_time, steps_per_sec, steps_per_sec * 3600 / 1000000.))
-
       self.local_t = 0
 
-      if USE_LSTM:
-        self.local_network.reset_state()
+      self.actions = [] 
+      self.states = [] 
+      self.values = [] 
+      self.rewards = [] 
+      self.durations = [] 
+      self.start_lstm_states = []
+      self.variable_snapshots = []
+
+      self.local_network.reset_state()
+    else:
+      self.restore_backup(sess)
 
     self.actions = self.actions[LOCAL_T_MAX:]
     self.states = self.states[LOCAL_T_MAX:]

async_deep_reinforce/constants.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import logging
 
-LOCAL_T_MAX = 40 # repeat step size
+LOCAL_T_MAX = 20 # repeat step size
 RMSP_ALPHA = 0.99 # decay parameter for RMSProp
 RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
 CHECKPOINT_DIR = 'checkpoints'
@@ -14,15 +14,11 @@ PRECISION = tf.float32
 
 INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99 # discount factor for rewards
-# ENTROPY_BETA = 0.01 # entropy regurarlization constant (discrete actions)
-# ENTROPY_BETA = 10**(-4) # entropy regurarlization constant (continuous actions)
-ENTROPY_BETA = 10**(-3) # entropy regurarlization constant (continuous actions)
+ENTROPY_BETA = 10**(-4) # entropy regurarlization constant (continuous actions)
 MAX_TIME_STEP = 10 * 10**7
 # GRAD_NORM_CLIP = 40.0 # gradient norm clipping
 GRAD_NORM_CLIP = float("inf") # gradient norm clipping
 USE_GPU = False # To use GPU, set True
-# USE_LSTM = True # True for A3C LSTM, False for A3C FF
-USE_LSTM = True
 N_LSTM_LAYERS = 3
 
 STATE_SIZE = 11
@@ -32,6 +28,6 @@ LAYER_NORMALIZATION = True
 
 ALPHA = 1.0
 BETA = 1.0
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 OFFSET = 0.5
 ACTOR_LOSS_FACTOR = 1e0

async_deep_reinforce/game_ac_network.py
@@ -124,6 +124,40 @@ class GameACNetwork(object):
 	def get_vars(self):
 		raise NotImplementedError()
 
+	def backup_vars(self, name=None):
+		self.backup_vars = list(map(lambda var: tf.Variable(tf.zeros(var.get_shape(), dtype=PRECISION)), self.get_vars()))
+		sync_ops = []
+
+		with tf.name_scope(name, "GameACNetwork", []) as name:
+			with tf.device(self._device):
+				for (src_var, dst_var) in zip(self.get_vars(), self.backup_vars):
+					sync_op = tf.assign(dst_var, src_var)
+					sync_ops.append(sync_op)
+
+				group = tf.group(*sync_ops, name=name)
+				def backup_vars_inner_function(sess):
+					self.backup_lstm_state = self.lstm_state
+					sess.run(group)
+				
+				return backup_vars_inner_function
+		
+	def restore_backup(self, name=None):
+		sync_ops = []
+
+		with tf.name_scope(name, "GameACNetwork", []) as name:
+			with tf.device(self._device):
+				for (src_var, dst_var) in zip(self.backup_vars, self.get_vars()):
+					sync_op = tf.assign(dst_var, src_var)
+					sync_ops.append(sync_op)
+
+				group = tf.group(*sync_ops, name=name)
+				def restore_backup_inner_function(sess):
+					self.lstm_state = self.backup_lstm_state
+					sess.run(group)
+
+				return restore_backup_inner_function
+
+
 	def sync_from(self, src_network, name=None):
 		src_vars = src_network.get_vars()
 		dst_vars = self.get_vars()
@@ -132,7 +166,7 @@ class GameACNetwork(object):
 
 		with tf.device(self._device):
 			with tf.name_scope(name, "GameACNetwork", []) as name:
-				for(src_var, dst_var) in zip(src_vars, dst_vars):
+				for (src_var, dst_var) in zip(src_vars, dst_vars):
 					sync_op = tf.assign(dst_var, src_var)
 					sync_ops.append(sync_op)
 
@@ -202,7 +236,7 @@ class GameACLSTMNetwork(GameACNetwork):
 
 			# weight for policy output layer
 			self.W_hidden_to_action_mean_fc, self.b_hidden_to_action_mean_fc = self._fc_variable([HIDDEN_SIZE, 1])
-			self.W_hidden_to_action_std_fc, self.b_hidden_to_action_std_fc = self._fc_variable([HIDDEN_SIZE, 1])
+			# self.W_hidden_to_action_std_fc, self.b_hidden_to_action_std_fc = self._fc_variable([HIDDEN_SIZE, 1])
 
 			# weight for value output layer
 			self.W_hidden_to_value_packets_fc, self.b_hidden_to_value_packets_fc = self._fc_variable([HIDDEN_SIZE, 1])
@@ -242,12 +276,12 @@ class GameACLSTMNetwork(GameACNetwork):
 			
 
 			raw_pi_mean = tf.matmul(lstm_outputs, self.W_hidden_to_action_mean_fc) + self.b_hidden_to_action_mean_fc
-			raw_pi_std = tf.matmul(lstm_outputs, self.W_hidden_to_action_std_fc) + self.b_hidden_to_action_std_fc
+			# raw_pi_std = tf.matmul(lstm_outputs, self.W_hidden_to_action_std_fc) + self.b_hidden_to_action_std_fc
 			# policy (output)
 			self.pi = (
 				tf.nn.softplus(raw_pi_mean) + 1.0,
-				tf.nn.softplus(raw_pi_std) + 1.0
-				# tf.constant(1.0, shape=(1,), dtype=PRECISION)
+				# tf.nn.softplus(raw_pi_std) + 1.0
+				tf.constant(1.0, shape=(1,), dtype=PRECISION)
 			)
 
 			# value (output)
@@ -278,8 +312,6 @@ class GameACLSTMNetwork(GameACNetwork):
 			[self.pi, self.v_packets, self.v_accumulated_delay, self.v_duration, self.lstm_state],
 			feed_dict = {self.s : [s_t],
 			self.initial_lstm_state : self.lstm_state_out,
-			# self.initial_lstm_state : self.lstm_state_out[0],
-			# self.initial_lstm_state1 : self.lstm_state_out[1],
 			self.step_size : [1]} 
 		)
 		# pi_out: (1,3), v_out: (1)
@@ -292,28 +324,15 @@ class GameACLSTMNetwork(GameACNetwork):
 			[self.pi, self.chosen_action, self.v_packets, self.v_accumulated_delay, self.v_duration, self.lstm_state],
 			feed_dict = {self.s : [s_t],
 			self.initial_lstm_state : self.lstm_state_out,
-			# self.initial_lstm_state : self.lstm_state_out[0],
-			# self.initial_lstm_state1 : self.lstm_state_out[1],
 			self.step_size : [1]} 
 		)
 		# pi_out: (1,3), v_out: (1)
 		return ((pi_out[0][0], pi_out[1][0]), action_out[0],(v_packets_out[0], v_accumulated_delay_out[0], v_duration_out[0]))
 
-	# def run_policy(self, sess, s_t):
-	# 	# This run_policy() is used for displaying the result with display tool.    
-	# 	pi_out, self.lstm_state_out = sess.run( [self.pi, self.lstm_state],
-	# 												feed_dict = {self.s : [s_t],
-	# 												self.initial_lstm_state : self.lstm_state_out,
-	# 												#  self.initial_lstm_state0 : self.lstm_state_out[0],
-	# 												#  self.initial_lstm_state1 : self.lstm_state_out[1],
-	# 												self.step_size : [1]} )
-
-	# 	return (pi_out[0][0], pi_out[1][0])
-
 	def run_value(self, sess, s_t):
 		# This run_value() is used for calculating V for bootstrapping at the 
 		# end of LOCAL_T_MAX time step sequence.
-		# When next sequcen starts, V will be calculated again with the same state using updated network weights,
+		# When next sequence starts, V will be calculated again with the same state using updated network weights,
 		# so we don't update LSTM state here.
 		prev_lstm_state_out = self.lstm_state_out
 		v_packets_out, v_accumulated_delay_out, v_duration_out, _ = sess.run( [self.v_packets, self.v_accumulated_delay, self.v_duration, self.lstm_state],
@@ -327,27 +346,16 @@ class GameACLSTMNetwork(GameACNetwork):
 		self.lstm_state_out = prev_lstm_state_out
 		return (v_packets_out[0], v_accumulated_delay_out[0], v_duration_out[0])
 
-	def run_loss(self, sess, si, ai, td_throughput, td_delay, r_duration, r_packets, r_accumulated_delay):
-		# This run_value() is used for calculating V for bootstrapping at the 
-		# end of LOCAL_T_MAX time step sequence.
-		# When next sequcen starts, V will be calculated again with the same state using updated network weights,
-		# so we don't update LSTM state here.
-		prev_lstm_state_out = self.lstm_state_out
-		entropy, action, value, total, window, std, _ = sess.run( [self.entropy, self.actor_loss, self.value_loss, self.total_loss, self.distribution_mean, self.distribution_std, self.lstm_state],
-								feed_dict = {self.s : [si], self.a: [ai], self.td_throughput: [td_throughput], self.td_delay: [td_delay], self.r_duration: [r_duration], self.r_packets: [r_packets], self.r_accumulated_delay: [r_accumulated_delay],
-								self.initial_lstm_state : self.lstm_state_out,
-								# self.initial_lstm_state0 : self.lstm_state_out[0],
-								# self.initial_lstm_state1 : self.lstm_state_out[1],
-								self.step_size : [1]} )
+	def run_loss(self, sess, feed_dict):
+		# We don't have to roll back the LSTM state here as it is restored in the "process" function of a3c_training_thread.py anyway and because run_loss is only called there. 
+		entropy, action, value, total, window, std = sess.run( [self.entropy, self.actor_loss, self.value_loss, self.total_loss, self.distribution_mean, self.distribution_std], feed_dict = feed_dict )
 		
-		# roll back lstm state
-		self.lstm_state_out = prev_lstm_state_out
 		return entropy, action, value, total, window + OFFSET, std
 
 	def get_vars(self):
 		return [self.W_state_to_hidden_fc, self.b_state_to_hidden_fc,
 						self.W_hidden_to_action_mean_fc, self.b_hidden_to_action_mean_fc,
-						self.W_hidden_to_action_std_fc, self.b_hidden_to_action_std_fc,
+						# self.W_hidden_to_action_std_fc, self.b_hidden_to_action_std_fc,
 						self.W_hidden_to_value_packets_fc, self.b_hidden_to_value_packets_fc,
 						self.W_hidden_to_value_delay_fc, self.b_hidden_to_value_delay_fc,
 						self.W_hidden_to_value_duration_fc, self.b_hidden_to_value_duration_fc] + self.LSTM_variables