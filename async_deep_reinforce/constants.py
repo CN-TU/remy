ef7dde8cc66f4e1d1c5a033d455b5b0d31f9cce5
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2017-09-29 19:23:44 +0200

    Partly adapt Python code to uncooperative training

--- async_deep_reinforce/constants.py
+++ async_deep_reinforce/constants.py
@@ -13,7 +13,7 @@ RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
 ABSOLUTE_PATH = os.path.join(os.path.expanduser('~'),"repos/remy/")
 CHECKPOINT_DIR = ABSOLUTE_PATH+'checkpoints'
 LOG_FILE = ABSOLUTE_PATH+'tmp/a3c_log'
-LEARNING_RATE_MULTIPLIER = 1e-1
+LEARNING_RATE_MULTIPLIER = 1e-2
 INITIAL_ALPHA_LOW = 1e-4*LEARNING_RATE_MULTIPLIER    # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e-2*LEARNING_RATE_MULTIPLIER   # log_uniform high limit for learning rate
 
