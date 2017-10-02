Bug fixes and decrease precision to float instead of double

async_deep_reinforce/constants.py
@@ -17,7 +17,7 @@ LEARNING_RATE_MULTIPLIER = 0.5e-1
 INITIAL_ALPHA_LOW = 1e-4*LEARNING_RATE_MULTIPLIER    # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e-2*LEARNING_RATE_MULTIPLIER   # log_uniform high limit for learning rate
 
-PRECISION = tf.float64
+PRECISION = tf.float32
 
 INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99 # discount factor for rewards
@@ -26,7 +26,7 @@ STD_BIAS_OFFSET = -3
 MAX_TIME_STEP = 10**7
 # GRAD_NORM_CLIP = 40.0 # gradient norm clipping
 USE_GPU = False # To use GPU, set True
-N_LSTM_LAYERS = 4
+N_LSTM_LAYERS = 3
 
 STATE_SIZE = 11
 HIDDEN_SIZE = 256