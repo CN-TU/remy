eda248c613c43ee5d19e33c46917fc7345906e1e
Maximilian Bachl <maximilian.bachl@tuwien.ac.at>
2017-10-06 19:23:23 +0200

    Improved?

--- async_deep_reinforce/constants.py
+++ async_deep_reinforce/constants.py
@@ -19,7 +19,7 @@ else:
 LOG_FILE = ABSOLUTE_PATH+'tmp/a3c_log'
 ACTOR_FACTOR = 1e-6
 VALUE_FACTOR = 1e0
-GENERAL_FACTOR = 1e0
+GENERAL_FACTOR = 1e-2
 INITIAL_ALPHA_LOW = 1e-2*GENERAL_FACTOR   # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e0*GENERAL_FACTOR   # log_uniform high limit for learning rate
 
@@ -27,8 +27,8 @@ PRECISION = tf.float32
 
 INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99 # discount factor for rewards
-ENTROPY_BETA = 1e-4
-STD_BIAS_OFFSET = -2
+ENTROPY_BETA = 1e-5
+STD_BIAS_OFFSET = -1
 MAX_TIME_STEP = 1e9
 # GRAD_NORM_CLIP = 40.0 # gradient norm clipping
 USE_GPU = False # To use GPU, set True
