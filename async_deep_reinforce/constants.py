5f2eb217875def351549a2c6d814be19c9c6ce22
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2017-10-08 00:34:26 +0200

    Parameter tuning

--- async_deep_reinforce/constants.py
+++ async_deep_reinforce/constants.py
@@ -17,9 +17,9 @@ if environ.get('checkpoints') is not None:
 else:
 	CHECKPOINT_DIR = ABSOLUTE_PATH+'checkpoints'
 LOG_FILE = ABSOLUTE_PATH+'tmp/a3c_log'
-ACTOR_FACTOR = 1e-3
+ACTOR_FACTOR = 1e0
 VALUE_FACTOR = 1e0
-GENERAL_FACTOR = 1e-3
+GENERAL_FACTOR = 1e-6
 INITIAL_ALPHA_LOW = 1e-2*GENERAL_FACTOR   # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e0*GENERAL_FACTOR   # log_uniform high limit for learning rate
 
